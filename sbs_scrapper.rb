aa658d740d23d2192e9db4bf0f46824fd570673c
Julio Castillo <julrocas@gmail.com>
2024-04-23 17:59:01 -0500

    ga

--- sbs_scrapper.rb
+++ sbs_scrapper.rb
@@ -3,11 +3,29 @@ require 'httparty'
 require 'nokogiri'
 require 'json'
 
+def fetch_data_with_retry(url, max_retries: 3, wait_factor: 2)
+  retries = 0
+  wait_time = 1
+
+  begin
+    response = HTTParty.get(url)
+    return response if response.success?
+  rescue HTTParty::Error, StandardError => e
+    retries += 1
+    raise e if retries >= max_retries
+
+    puts "Error occurred: #{e.message}. Retrying in #{wait_time} seconds..."
+    sleep(wait_time)
+    wait_time *= wait_factor
+    retry
+  end
+end
+
 today = Date.today.strftime("%d/%m/%Y")
 one_month_ago = Date.today.prev_month.strftime("%d/%m/%Y")
 
 url = "https://www.sbs.gob.pe/app/stats/seriesH-tipo_cambio_moneda_excel.asp?fecha1=#{one_month_ago}&fecha2=#{today}&moneda=02&cierre="
-response = HTTParty.get(url)
+response = fetch_data_with_retry(url)
 
 doc = Nokogiri::HTML(response)
 parsed_data = []
@@ -20,6 +38,6 @@ doc.xpath('//table/tr[position() > 1]').each do |row|
   parsed_data << { date: date, buy: buy, sell: sell }
 end
 
-json_data = parsed_data.to_json
+json_data = parsed_data.to_json + "\n"
 
 File.open('sbs_data.json', 'w') { |file| file.write(json_data) }
